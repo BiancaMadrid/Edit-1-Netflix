2c0d0225d46b038e20d5e227e0b66735b5e1d100
BiancaMadrid <bugamadrid111@gmail.com>
2024-09-24 14:10:22 -0700

    SAFE POINT

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,37 @@ public class Main {
         String firstName ="Bianca";
         int age = 23;
 
+        System.out.println("Hello user!");
+        String username = "Bugambilia";
+        String password = "thisisreallyhard123";
+        String phoneNumber = "480-658-7199";
+        String personalEmail = "bugamadrid111@gmail.com";;
+
+        String movieTitle = "Get Out";
+        int releaseYear = 2023;
+        float movieRating = 10.00f;
+        boolean movieFavorite = true;
+        double movieReviews = 9999999999999d;
+        String directorsName =  "Jordan Peel";
+
+
+        /*to add new line to code as /n in the code and rewrite it
+         Example: Systems.out.println("Hello/n");
+         would run as:
+         Hello
+         world
+         */
+
+        System.out.println("Hello user!" + " " + username );
+        System.out.println("Password" + " " + password);
+        System.out.println( "Select" + " " + movieTitle);
+        System.out.println( "Release year" + " " + releaseYear);
+        System.out.println( "Movie Rating" + " " + movieRating);
+        System.out.println("Is this your favorite Movie?" + " " + movieFavorite);
+        System.out.println("Number of Reviews" + " " + movieReviews);
+        System.out.println("Directors Name" + " " + directorsName);
+
+
 
 
     }
